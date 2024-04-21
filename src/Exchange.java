18aa9f6bb1d9ff39107413bfe7a789420830ad6f
Yaojema <yaojema@gmail.com>
2024-04-21 13:21:26 -0500

    Creado conversor de monedas desde la API

---
+++ src/Exchange.java
@@ -0,0 +1,8 @@
+
+public record Exchange(String result,
+                       String base_code,
+                       String target_code,
+                       String conversion_rate,
+                       String conversion_result
+                       ) {
+}
--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,42 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("Hello World! v2");
+        System.out.println("Bienvenido a compara ratio");
+
+        Scanner lectura = new Scanner(System.in);
+
+        ConsultaExchange consulta = new ConsultaExchange();
+
+        try {
+            System.out.println("****************************************");
+            System.out.println("--- CONVERSOR DE DIVISAS ---");
+            System.out.println("****************************************");
+            System.out.println("Escriba el par de divisas, a convertir -> ejemplo USD -> EUR");
+            System.out.println("Ingresar divisa base");
+            var baseDivisa = lectura.nextLine();
+            System.out.println("Ingresar divisa target");
+            var targetDivisa = lectura.nextLine();
+            System.out.println("Ingresar monto a convertir");
+            var monto = lectura.nextInt();
+            // cast
+//            var monto = Integer.valueOf(lectura.nextLine());
+
+            Exchange exchange = consulta.buscarPar(baseDivisa, targetDivisa, monto);
+            System.out.println("De: " + exchange.base_code() + " >> " + exchange.target_code() + " Es:");
+            System.out.println(exchange.conversion_result());
+
+            System.out.println("****************************************");
+//            GeneradorDeArchivo generador = new GeneradorDeArchivo();
+//            generador.guardarJson(pelicula);
+
+        } catch ( NumberFormatException e){
+            System.out.println("No encontrado " + e.getMessage());
+        } catch (RuntimeException e){
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando la aplicacion");
+        }
+
     }
 }
